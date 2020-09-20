92f9be65539c8243f415127be8d0fb05474f6ab1
RedOne23 <42481758+RedOne23@users.noreply.github.com>
2020-09-20 13:40:16 +0200

    cookie_generatorJS

---
+++ app.js
@@ -0,0 +1,117 @@
+ const affichage = document.querySelector('.affichage');
+ const btns = document.querySelectorAll('button');
+ const inputs = document.querySelectorAll('input');
+ const infoTxt = document.querySelector('.info-txt');
+ let dejaFait = false;
+
+ 
+ 
+ const today = new Date();
+ const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000); // Permet d'ajouter 7 jours à la date du jour
+
+ let day = ('0' + nextWeek).slice(9,11); // Permet de transformer l'objet date en chaine de caractère
+ let month = ('0' + (today.getMonth() + 1)).slice(-2); // Permet d'obtenir le mois , l'objet date débute les mois à 0 (d'où le +1)
+ let year = today.getFullYear();
+ document.querySelector('input[type=date]').value = `${year}-${month}-${day}`;
+
+btns.forEach(btn => {
+  btn.addEventListener('click', btnAction);
+})
+
+function btnAction(e){
+  
+  let nvObj = {};
+
+  inputs.forEach(input => {
+    let attrName = input.getAttribute('name');
+    let attrValeur = attrName !== "cookieExpire" ? input.value : input.valueAsDate;
+    nvObj[attrName] = attrValeur;
+  })
+
+  let description = e.target.getAttribute('data-cookie');
+
+  if(description === "creer"){
+    creerCookie(nvObj.cookieName, nvObj.cookieValue, nvObj.cookieExpire)
+  } else if(description === "toutAfficher") {
+    listeCookies();
+  }
+
+}
+
+function creerCookie(name, value, exp){
+
+    infoTxt.innerText = "";
+    affichage.innerHTML = "";
+
+  // Si le cookie a un même nom
+
+  let cookies = document.cookie.split(';');
+  cookies.forEach(cookie => {
+
+    cookie = cookie.trim();
+    // console.log(cookie);
+    let formatCookie = cookie.split('=');
+    console.log(formatCookie)
+    if(formatCookie[0] === encodeURIComponent(name)){
+        dejaFait = true;
+    }
+
+  })
+
+  if(dejaFait){
+    infoTxt.innerText = "Un cookie possède déjà ce nom."
+    dejaFait = false;
+    return;
+  }
+
+  // Si le cookie n'a pas de nom
+
+  if(name.length === 0){
+    infoTxt.innerText = `Impossible de définir un cookie sans nom.`
+    return;
+  }
+
+  document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)};expires=${exp.toUTCString()}`;
+  let info = document.createElement('li');
+  info.innerText = `Cookie ${name} créé.`
+  affichage.appendChild(info);
+  setTimeout(() => {
+    info.remove();
+  }, 1500);
+}
+
+function listeCookies(){
+
+  let cookies = document.cookie.split(';');
+  if(cookies.join() === "") {
+      infoTxt.innerText = "Pas de cookie à afficher";
+      return;
+  }
+
+  cookies.forEach(cookie => {
+    cookie = cookie.trim();
+    let formatCookie = cookie.split('=');
+
+    // console.log(formatCookie);
+
+    let item = document.createElement('li');
+
+    infoTxt.innerText = 'Cliquez sur un cookie dans la liste pour le supprimer'
+    item.innerText = `Nom : ${decodeURIComponent(formatCookie[0])}, Valeur : ${decodeURIComponent(formatCookie[1])}`;
+    affichage.appendChild(item);
+
+
+    // suppression cookie
+
+    item.addEventListener('click', () =>{
+
+        document.cookie = `${formatCookie[0]}=; expires=${new Date(0)}`
+        item.innerText = `cookie ${formatCookie[0]} supprimé`;
+        setTimeout(() => {
+          item.remove();
+        }, 1000);
+    })
+
+})
+
+}
